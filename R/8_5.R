c5befa92ffeae3634c49c56481a3106089b38f2d
hmst2020 <60832331+hmst2020@users.noreply.github.com>
2023-12-31 19:29:19 +0800

    Add files via upload

--- R/8_5.R
+++ R/8_5.R
@@ -28,4 +28,21 @@ dimnames(T) <- list(     # 對矩陣給予列名、行名
 ####方法三########
 (eig<-eigen(T))          # 特徵分解
 eig$vectors[,1]/sum(eig$vectors[,1])   # 主特徵向量比值
+############# 方法 四################
+library(expm)
+(.Machine$double.eps^0.5) # all.equal 精度要求
+(v0 <- c(0.80,0.15,0.05)) # 初始分布比例
+for (i in 0:300) {     
+  w <- T %*% v0         # 迭代轉換
+  vRatios<-v0/min(v0)   # 迭代前比例
+  wRatios<-w/min(w)     # 迭代後比例
+  if (isTRUE(all.equal(wRatios,vRatios))){  # 判斷迭代前後是否相同
+    break    # 若相同，結束迴圈
+  }
+  v0<-w      # 給予迭代值
+}
+print(i-1)               # 穩定狀態經過迭代次數
+print(v0)                # 穩定狀態時的比例分布
+print(T%^%(i-1))         # 極限矩陣
+print(colSums(T%^%(i-1))) # 行向量和
 ############### end of 8_5.R ############
